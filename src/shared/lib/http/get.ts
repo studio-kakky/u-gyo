9148e2da00d1381b9c6e2c3a7712503f5cb2c289
studio-kakky <info@studio-kakky.com>
2022-09-19 13:36:10 +0900

    get関数追加

--- src/shared/lib/http/get.ts
+++ src/shared/lib/http/get.ts
@@ -1,3 +1,17 @@
 import { GetHttpParams } from "../../models/http/get-http-params";
+import { makeUrl } from "./make-url";
 
-export const get = (endpointUrl: string, params: GetHttpParams) => {};
+export const httpGet = <T>(
+  endpointUrl: string,
+  params: GetHttpParams
+): Promise<T> => {
+  const search = params.searchParams
+    ? `?${params.searchParams.toString()}`
+    : "";
+  const url = `${makeUrl(endpointUrl, params)}${search}`;
+  return fetch(url, {
+    headers: params.headers,
+  }).then((res) => {
+    return res.json();
+  });
+};
