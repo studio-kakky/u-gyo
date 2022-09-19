fae844c90c6993444c8a3a590fe513c3886624bb
studio-kakky <info@studio-kakky.com>
2022-09-19 15:32:14 +0900

    swrを利用してfetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import useSWR from "swr";
 import styles from "../styles/Home.module.css";
+import { createShipsFetcher } from "../shared/interactors/ships";
 
 const Home: NextPage = () => {
+  const { fetcher, cacheKey } = createShipsFetcher();
+  const { data: ships } = useSWR(cacheKey, fetcher);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,7 +15,7 @@ const Home: NextPage = () => {
         <meta name="description" content="U魚" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <pre>{JSON.stringify(ships)}</pre>
       <main className={styles.main}>
         <h1 className={styles.title}>U魚</h1>
       </main>
