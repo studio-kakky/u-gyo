64b95b463efc48cfd74b7af42c327e59fda2451e
studio-kakky <info@studio-kakky.com>
2022-09-27 17:20:59 +0900

    地図表示

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { ShipsList } from './ships-list';
 import { SearchBox } from '../../shared/search-box';
 import styles from './index.module.scss';
+import { AppMap } from '../../shared/map/map';
 
 export const HomePage = (): JSX.Element => {
   const [searchWords, setSearchWords] = useState<string[]>([]);
@@ -16,8 +17,13 @@ export const HomePage = (): JSX.Element => {
       <div className={styles.Layout_util}>
         <SearchBox setSearchWords={setSearchWords} />
       </div>
-      <div className={styles.Layout_shipList}>
-        <ShipsList searchWords={searchWords} />
+      <div className={styles.Layout_content}>
+        <div className={styles.Layout_map}>
+          <AppMap />
+        </div>
+        <div className={styles.Layout_shipList}>
+          <ShipsList searchWords={searchWords} />
+        </div>
       </div>
     </>
   );
--- src/components/shared/map/map.tsx
+++ src/components/shared/map/map.tsx
@@ -1,3 +1,25 @@
+import React from 'react';
+import DeckGL from '@deck.gl/react/typed';
+import { LineLayer } from '@deck.gl/layers/typed';
+import { Map } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0,
+};
+
+const data = [{ sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781] }];
+
 export const AppMap = (): JSX.Element => {
-  return <></>;
+  const layers = [new LineLayer({ id: 'line-layer', data })];
+
+  return (
+    <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={layers}>
+      <Map mapStyle="mapbox://styles/mapbox/outdoors-v11" mapboxAccessToken={process.env.NEXT_PUBLIC_MAP_BOX_TOKEN} />
+    </DeckGL>
+  );
 };
